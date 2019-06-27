c91c48fdb7909a8c1209f73998ced61e1189e744
Miguel Marin <mmarin@yoobic.com>
2019-06-27 09:58:45 +0100

    chore(config): add support for sass

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -2,7 +2,7 @@ import { Component, Prop, h } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
-  styleUrl: 'my-component.css',
+  styleUrl: 'my-component.scss',
   shadow: true
 })
 export class MyComponent {
@@ -11,6 +11,6 @@ export class MyComponent {
   @Prop() result: string;
 
   render() {
-    return <div>Hello. My name is {this.name}. You {this.action}. Prepare to {this.result}.</div>;
+    return <div class="name">Hello. My name is {this.name}. You {this.action}. Prepare to {this.result}.</div>;
   }
 }
